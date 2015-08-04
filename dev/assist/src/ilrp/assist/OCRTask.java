2a9f699dd515f7dfac5041b9a364e0f11c4332f8
Haibara <zhenlei.yan@gmail.com>
2015-08-04 10:36:10 +0800

    add new files

--- dev/assist/src/ilrp/assist/OCRTask.java
+++ dev/assist/src/ilrp/assist/OCRTask.java
@@ -1,6 +1,14 @@
 package ilrp.assist;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
@@ -10,10 +18,17 @@ public class OCRTask implements Runnable{
 
 	private BufferedImage image = null;
 	private static ITesseract tess = new Tesseract();
+	static {
+		 tess.setTessVariable("tessedit_char_whitelist", ".0123456789");
+	}
 	private String ocrRet = "";
 	
 	public OCRTask(BufferedImage image) {
 		this.image = image;
+	}
+	
+	public OCRTask() {
+		
 	}
 	
 	@Override
@@ -23,8 +38,97 @@ public class OCRTask implements Runnable{
 		} catch (TesseractException e) {
 			e.printStackTrace();
 		}
-		ocrRet = ocrRet.substring(0, ocrRet.length() - 1);
 		System.out.println(ocrRet);
 	}
+	
+	public static String recognize(BufferedImage image, boolean preProcessed) {
+		String ret = "";
+		Debuger.startTimer("ocr");
+		if (preProcessed) {
+			image = normalizedImage(image);
+		}
+//		try {
+//			ImageIO.write(image, "png", new File("D:\\github\\i-love-red-packet\\dev\\assist\\ocr.png"));
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+		long start = System.currentTimeMillis();
+		try {
+			ret = tess.doOCR(image).trim();
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
+		Debuger.stopTimer("ocr");
+		return ret;
+	}
+	
+	public static BufferedImage normalizedImage(BufferedImage image) {
+		binarizedImage(image);		
+		return shrinkImage(image, 0.5);
+	}
+	
+	public static BufferedImage shrinkImage(BufferedImage image, double scale) {
+		BufferedImage resizedImage = new BufferedImage((int)(image.getWidth()*scale), (int)(image.getHeight()*scale), image.getType());
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(image, 0, 0, resizedImage.getWidth(), resizedImage.getHeight(), null);
+
+		if (scale > 1.0) {
+			g.setComposite(AlphaComposite.Src);
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		}
+		g.dispose();
+		
+		return resizedImage;
+	}
+	
+	private static int blankRGB = (new Color(255,255,255)).getRGB();
+	private static int blackRGB = (new Color(0,0,0)).getRGB();
+	
+	public static BufferedImage binarizedImage(BufferedImage image) {
+		for (int i = 0; i < image.getWidth(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				if (image.getRGB(i, j) != blackRGB) {
+					image.setRGB(i, j, blankRGB);
+				}
+			}
+		}
+		return image;		
+	}
+	
+	public static void main(String... args) {
+		OCRTask ot = new OCRTask();
+		try {
+			BufferedImage image = ImageIO.read(new File("D:\\github\\i-love-red-packet\\dev\\assist\\ocr.png"));
+			long start = System.currentTimeMillis();
+//			ot.binarizedImage(image);
+			System.out.println(ot.recognize(image,false));
+			long end = System.currentTimeMillis();
+			System.out.println("ocr gap:"+((end-start)));
+			start = System.currentTimeMillis();
+			ot.binarizedImage(image);
+			end = System.currentTimeMillis();
+			System.out.println("binarize gap:"+(end-start));
+			ImageIO.write(image, "png", new File("D:\\github\\i-love-red-packet\\dev\\assist\\binary.png"));
+			start = System.currentTimeMillis();
+			System.out.println(ot.recognize(image,false));
+			end = System.currentTimeMillis();
+			System.out.println("binary ocr gap:"+((end-start)));
+			//
+			start = System.currentTimeMillis();
+			BufferedImage newImage = ot.shrinkImage(image,0.5);
+			end = System.currentTimeMillis();
+			System.out.println("shrink gap:"+(end-start));
+			ImageIO.write(newImage, "png", new File("D:\\github\\i-love-red-packet\\dev\\assist\\shrink.png"));
+			start = System.currentTimeMillis();
+			System.out.println(ot.recognize(newImage,false));
+			end = System.currentTimeMillis();
+			System.out.println("shrink ocr gap:"+((end-start)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 }
---
+++ dev/assist/src/ilrp/assist/Situation.java
@@ -0,0 +1,71 @@
+package ilrp.assist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Situation {
+	
+	private List<Float> packets = new ArrayList<Float>();
+	private float min = Float.MAX_VALUE;
+	private float max = Float.MIN_VALUE;
+	private float myPacket = -1;
+	private boolean isOver = false;
+	
+	public Situation () {
+		
+	}
+	
+	public void addRedpacket(float number) {
+		packets.add(number);
+		if (number <= min) {
+			min = number;
+		}
+		if (number >= max) {
+			max = number;
+		}
+	}
+	
+	public Float getMax() {
+		return this.max;
+	}
+	
+	public Float getMin() {
+		return this.min;
+	}
+	
+	public List<Float> getRedpackets() {
+		return this.packets;
+	}
+	
+	public int getPacketsCount() {
+		return this.packets.size();
+	}
+	
+	public void setMyPacket(float number) {
+		this.myPacket = number;
+	}
+	
+	public float getMyPacket() {
+		return this.myPacket;
+	}
+	
+	public void setOver(boolean over) {
+		this.isOver = over;
+	}
+	
+	public boolean getOver() {
+		return this.isOver;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("My packet:"+this.myPacket+"\n");
+		for (float number : packets) {
+			sb.append(number + " ");
+		}
+		sb.append("\n");
+		sb.append("min packet:"+this.min + "\n");
+		sb.append("max packet:"+this.max+"\n");
+		return sb.toString();
+	}
+}
